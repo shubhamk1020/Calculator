7794b42a5603e6fecac4df2b90e55fcaf8009ede
shubham1020k <gyaninfo128@gmail.com>
2022-07-23 19:15:33 +0530

    Alert Dialog

--- app/src/main/java/com/mastercoding/calculator/MainActivity.java
+++ app/src/main/java/com/mastercoding/calculator/MainActivity.java
@@ -2,11 +2,14 @@ package com.mastercoding.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -88,11 +91,53 @@ public class MainActivity extends AppCompatActivity {
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-               finish();
+            new AlertDialog.Builder(MainActivity.this)
+                    .setIcon(R.drawable.ic_baseline_warning_24)
+                    .setTitle("Exit")
+                    .setMessage("Are you sure you want to exit this app")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    }).setNeutralButton("Help", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(MainActivity.this, "Press Yes to exit this app", Toast.LENGTH_SHORT).show();
+                }
+            }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).show();
             }
         });
 
 
     }
-}
+
+    @Override
+    public void onBackPressed() {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setIcon(R.drawable.ic_baseline_warning_24)
+                    .setTitle("Exit")
+                    .setMessage("Are you sure you want to exit this app")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    }).setNeutralButton("Help", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(MainActivity.this, "Press Yes to exit this app", Toast.LENGTH_SHORT).show();
+                }
+            }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).show();
+        }
+    }
